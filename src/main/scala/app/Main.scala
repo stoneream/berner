b77c5bbe2047ed64f0506bb638c1729ea36ab48d
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2023-07-22 02:22:40 +0900

    あぼーん

--- src/main/scala/app/Main.scala
+++
@@ -1,3 +0,0 @@
-package app
-
-object Main extends App {}
--- src/test/scala/app/MainSpec.scala
+++
@@ -1,9 +0,0 @@
-package app
-
-import org.scalatest.funsuite.AnyFunSuite
-
-class MainSpec extends AnyFunSuite {
-  test("sample test") {
-    // test here
-  }
-}
