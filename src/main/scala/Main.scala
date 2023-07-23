a10a051373cbfe60c67b6a14fcbb4aa868e10ea7
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2023-07-24 03:08:12 +0900

    コンフィグファイルからトークンを読み込む

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,47 +12,47 @@ import java.net.http.HttpClient
 import scala.concurrent.duration.*
 
 object Main extends IOApp {
-  // todo コンフィグファイルからトークンを読み込む
-  private val token = "server-token-here"
   // todo gateway api 叩いて url を取得する
   private val gatewayUri = uri"wss://gateway.discord.gg/?v=10&encoding=json"
 
   // todo 再接続処理
   override def run(args: List[String]): IO[ExitCode] = {
-    JdkWSClient[IO](HttpClient.newHttpClient)
-      .connectHighLevel(WSRequest(gatewayUri))
-      .use { client =>
-        val identity = Payload[Identity](2, Some(Identity(token, 513, Map())), None, None)
-        val payload = WSFrame.Text(identity.asJson.noSpaces)
-        client.send(payload).flatMap { _ =>
-          client.receiveStream
-            .collectFirst({ case WSFrame.Text(text, _) => text })
-            .flatMap { text =>
-              parse(text) match {
-                case Left(_) => Stream.unit
-                case Right(json) =>
-                  val op = json.hcursor.downField("op").as[Int].getOrElse(-1)
-                  Stream.emit(json).covary[IO].evalTap(json => IO(println(json.noSpaces))).flatMap { _ =>
-                    op match {
-                      case 10 => // hello
-                        val interval = json.hcursor.downField("d").downField("heartbeat_interval").as[Int].getOrElse(-1)
-                        val heartbeat = WSFrame.Text("""{"op": 1, "d": null}""")
-                        val heartbeatStream = Stream.awakeEvery[IO](interval.millis).evalMap { _ => client.send(heartbeat) }
-                        heartbeatStream.concurrently(client.receiveStream.evalMap {
-                          case WSFrame.Text(text, _) => IO.println(text)
-                          case _ => IO.unit
-                        })
-                      case _ =>
-                        Stream.unit
+    Config.apply.use { config =>
+      JdkWSClient[IO](HttpClient.newHttpClient)
+        .connectHighLevel(WSRequest(gatewayUri))
+        .use { client =>
+          val identity = Payload[Identity](2, Some(Identity(config.discordToken, 513, Map())), None, None)
+          val payload = WSFrame.Text(identity.asJson.noSpaces)
+          client.send(payload).flatMap { _ =>
+            client.receiveStream
+              .collectFirst({ case WSFrame.Text(text, _) => text })
+              .flatMap { text =>
+                parse(text) match {
+                  case Left(_) => Stream.unit
+                  case Right(json) =>
+                    val op = json.hcursor.downField("op").as[Int].getOrElse(-1)
+                    Stream.emit(json).covary[IO].evalTap(json => IO(println(json.noSpaces))).flatMap { _ =>
+                      op match {
+                        case 10 => // hello
+                          val interval = json.hcursor.downField("d").downField("heartbeat_interval").as[Int].getOrElse(-1)
+                          val heartbeat = WSFrame.Text("""{"op": 1, "d": null}""")
+                          val heartbeatStream = Stream.awakeEvery[IO](interval.millis).evalMap { _ => client.send(heartbeat) }
+                          heartbeatStream.concurrently(client.receiveStream.evalMap {
+                            case WSFrame.Text(text, _) => IO.println(text)
+                            case _ => IO.unit
+                          })
+                        case _ =>
+                          Stream.unit
+                      }
                     }
-                  }
+                }
               }
-            }
-            .compile
-            .drain
+              .compile
+              .drain
+          }
         }
-      }
-      .as(ExitCode.Success)
+        .as(ExitCode.Success)
+    }
   }
 
   private case class Payload[T](op: Int, d: Option[T], s: Option[Int], t: Option[String])
