4da831c77782239c07182682002455477f6f9930
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2023-07-28 00:35:27 +0900

    整理、続き

--- src/main/scala/discord/GatewayClient.scala
+++ src/main/scala/discord/GatewayClient.scala
@@ -2,28 +2,48 @@ package discord
 
 import application.Config
 import cats.effect.*
-import cats.syntax.all.*
 import cats.effect.std.Queue
+import cats.implicits.*
+import cats.syntax.all.*
 import discord.payload.{Identity, Payload}
-import fs2.Pipe
+import fs2.{Pipe, Stream}
+import io.circe.Json
 import io.circe.generic.auto.*
 import io.circe.parser.*
 import io.circe.syntax.*
 import org.http4s.client.websocket.{WSConnectionHighLevel, WSDataFrame, WSFrame, WSRequest}
 import org.http4s.implicits.uri
 import org.http4s.jdkhttpclient.JdkWSClient
-import fs2.Stream
+
 import scala.concurrent.duration.*
 
+// todo logging
 object GatewayClient {
 
   // todo gateway api 叩いて url を取得する
   private val gatewayUri = uri"wss://gateway.discord.gg/?v=10&encoding=json"
 
+  def run(config: Config): IO[Unit] = {
+    JdkWSClient.simple[IO].flatMap { client =>
+      client.connectHighLevel(WSRequest(gatewayUri)).use { connection =>
+        for {
+          jobQueue <- Queue.unbounded[IO, Json]
+          _ <- sendIdentity(connection, config.discordToken)
+          interval <- receiveHeartbeatInterval(connection)
+          _ <- (
+            sendHeartbeat(connection, interval),
+            offerReceiveEvent(connection, jobQueue),
+            Stream.fromQueueUnterminated(jobQueue).through(handleReceiveEvent).compile.drain
+          ).parTupled
+        } yield ()
+      }
+    }
+  }
+
   /**
    * `Gateway API`へ接続する
    */
-  private def sendIdentity[F[_]: Async](connection: WSConnectionHighLevel[F], token: String): F[Unit] = {
+  private def sendIdentity(connection: WSConnectionHighLevel[IO], token: String): IO[Unit] = {
     val payload = Payload(GatewayOpCode.Identify, Some(Identity(token, 513, Map())), None, None)
     connection.send(WSFrame.Text(payload.asJson.noSpaces))
   }
@@ -32,7 +52,7 @@ object GatewayClient {
    * `heartbeat interval`を受け取るまで待つ
    * ただし、30秒でタイムアウトする
    */
-  private def receiveHeartbeatInterval[F[_]: Async](connection: WSConnectionHighLevel[F]): F[Int] = {
+  private def receiveHeartbeatInterval(connection: WSConnectionHighLevel[IO]): IO[Int] = {
     connection.receiveStream
       .collectFirst({ case WSFrame.Text(text, _) =>
         parse(text)
@@ -49,9 +69,12 @@ object GatewayClient {
       .lastOrError
   }
 
-  private def sendHeartbeat[F[_]: Async](connection: WSConnectionHighLevel[F], interval: Int): F[Unit] = {
+  /**
+   * `heartbeat`を送信し続ける
+   */
+  private def sendHeartbeat[F[_] : Async](connection: WSConnectionHighLevel[F], interval: Int): F[Unit] = {
     Stream
-      .awakeEvery[F](interval.millis)
+      .awakeEvery(interval.millis)
       .evalMap { _ =>
         connection.send(WSFrame.Text("""{"op": 1, "d": null}"""))
       }
@@ -59,20 +82,18 @@ object GatewayClient {
       .drain
   }
 
-  private def handleReceiveEvent = ???
-
-  def run(config: Config): IO[Unit] = {
-    JdkWSClient.simple[IO].flatMap { client =>
-      client.connectHighLevel(WSRequest(gatewayUri)).use { connection =>
-        for {
-          _ <- sendIdentity(connection, config.discordToken)
-          interval <- receiveHeartbeatInterval(connection)
-          _ <- (
-            sendHeartbeat(connection, interval),
-            handleReceiveEvent
-          ).parTupled
-        } yield ()
-      }
-    }
+  /**
+   * `Gateway API`から受信したイベントをジョブキューに詰める
+   */
+  private def offerReceiveEvent(connection: WSConnectionHighLevel[IO], jobQueue: Queue[IO, Json]): IO[Unit] = {
+    connection.receiveStream.collect({ case WSFrame.Text(text, _) => parse(text) }).collect({ case Right(json) => json }).evalMap(jobQueue.offer).compile.drain
   }
+
+  /**
+   * ジョブキューを処理する
+   */
+  private def handleReceiveEvent: Pipe[IO, Json, Unit] = _.evalMap { json =>
+    IO.println(json.noSpaces)
+  }
+
 }
