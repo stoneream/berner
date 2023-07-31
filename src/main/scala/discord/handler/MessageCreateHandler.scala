ff1d2d137f8ad23d443d1aa6375dbf514794cd3d
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2023-07-31 23:38:35 +0900

    pingpongくらいはできるようになった

--- src/main/scala/discord/handler/MessageCreateHandler.scala
+++ src/main/scala/discord/handler/MessageCreateHandler.scala
@@ -2,10 +2,10 @@ package discord.handler
 
 import cats.effect.IO
 import discord.BotContext
-import io.circe.Json
+import io.circe._
 import io.circe.optics.JsonPath._
 import io.circe.syntax._
-import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
+import org.http4s.circe.CirceEntityCodec.{circeEntityDecoder, circeEntityEncoder}
 import org.http4s.{Headers, Method, Request, Uri, UrlForm}
 import org.http4s.jdkhttpclient.JdkHttpClient
 import org.typelevel.log4cats.slf4j.Slf4jLogger
@@ -21,45 +21,32 @@ object MessageCreateHandler {
           case Some(value) =>
             if (value != ctx.userId) {
               root.d.content.string.getOption(json) match {
-                case Some(content) =>
-                  val mentionPattern = "<@\\d+>".r
-                  val filteredMentionContent = mentionPattern.replaceAllIn(content, "")
-
-                  if (filteredMentionContent.contains("ping")) {
+                case Some(content) => {
+                  if (content.contains("ping")) {
                     root.d.channel_id.string.getOption(json) match {
                       case Some(channelId) =>
-                        JdkHttpClient
-                          .simple[IO]
-                          .map { client =>
-                            client
-                              .run(
-                                Request[IO](
-                                  method = Method.POST,
-                                  uri = Uri.unsafeFromString(s"https://discord.com/api/v10/channels/$channelId/messages"),
-                                  headers = Headers(
-                                    "Authorization" -> s"Bot ${ctx.token}"
-                                  )
-                                ).withEntity(
-                                  UrlForm(("content", "pong"))
-                                )
-                              )
-                              .use { response =>
-                                logger.info(s"response : ${response.status}")
-                              }
-                              .handleErrorWith { err =>
-                                logger.error(err)(s"failed to send message")
-                              }
-                          }
-                          .map { _ =>
-                            ctx
-                          }
+                        for {
+                          httpClient <- JdkHttpClient.simple[IO]
+                          request = Request[IO](
+                            method = Method.POST,
+                            uri = Uri.unsafeFromString(s"https://discord.com/api/v10/channels/$channelId/messages"),
+                            headers = Headers(
+                              "Authorization" -> s"Bot ${ctx.token}"
+                            )
+                          ).withEntity(
+                            UrlForm(("content", "pong"))
+                          )
+                          response <- httpClient.expect[Json](request)
+                        } yield {
+                          ctx
+                        }
                       case None =>
                         logger.info(s"channelId is not found") *> IO.pure(ctx)
                     }
                   } else {
                     IO.pure(ctx)
                   }
-
+                }
                 case None => IO.pure(ctx)
               }
             } else {
