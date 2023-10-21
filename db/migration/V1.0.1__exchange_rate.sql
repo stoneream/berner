fb2db805571fa5d4f23ca7d2633657f53828503f
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2023-10-21 16:24:53 +0900

    テーブル追加

---
+++ db/migration/V1.0.1__exchange_rate.sql
@@ -0,0 +1,12 @@
+-- todo コメント書きたい
+
+create table exchange_rates(
+  id serial primary key,
+  base_currency varchar(255) not null,
+  target_currency varchar(255) not null,
+  rate numeric(10, 6) not null,
+  target_date datetime not null,
+  created_at datetime not null,
+  updated_at datetime not null,
+  deleted_at datetime
+) engine = InnoDB default charset = utf8mb4;
