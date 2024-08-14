e32c170240a10c01be8ad04f69b897b2895cb982
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-08-15 01:25:00 +0900

    意識低め再起動

--- bot/src/main/scala/berner/bot/Main.scala
+++ bot/src/main/scala/berner/bot/Main.scala
@@ -1,11 +1,15 @@
 package berner.bot
 
+import berner.logging.Logger
 import net.dv8tion.jda.api.JDABuilder
 import com.typesafe.config.ConfigFactory
 import net.dv8tion.jda.api.requests.GatewayIntent
 import scalikejdbc.config.DBs
 
-object Main extends App {
+import scala.annotation.tailrec
+import scala.util.control.Exception.allCatch
+
+object Main extends App with Logger {
   try {
     val config = ConfigFactory.load()
 
@@ -13,17 +17,31 @@ object Main extends App {
 
     val discordBotToken = config.getString("discord.token")
 
-    val api = JDABuilder
-      .createDefault(discordBotToken)
-      .enableIntents(GatewayIntent.MESSAGE_CONTENT)
-      .enableIntents(GatewayIntent.GUILD_WEBHOOKS)
-      .enableIntents(GatewayIntent.GUILD_MESSAGES)
-      .addEventListeners(new Hub)
-      .addEventListeners(new Archiver)
-      .addEventListeners(new Ping)
-      .build()
+    @tailrec
+    def infinite(): Unit = {
+      allCatch.either {
+        val api = JDABuilder
+          .createDefault(discordBotToken)
+          .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+          .enableIntents(GatewayIntent.GUILD_WEBHOOKS)
+          .enableIntents(GatewayIntent.GUILD_MESSAGES)
+          .addEventListeners(new Hub)
+          .addEventListeners(new Archiver)
+          .addEventListeners(new Ping)
+          .build()
 
-    api.awaitShutdown()
+        api.awaitShutdown()
+      } match {
+        case Right(_) => // do nothing
+        case Left(e) =>
+          error("Botが異常終了", e)
+          info("3秒後に再起動")
+          Thread.sleep(3000)
+          infinite()
+      }
+    }
+
+    infinite()
   } finally {
     DBs.closeAll()
   }
