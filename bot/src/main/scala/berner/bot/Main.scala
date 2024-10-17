bb0405c70736e50d4f8f9fac66b3db0aedfd7469
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-10-18 01:17:41 +0900

    再起動を試みずに素直に落とす

--- bot/src/main/scala/berner/bot/Main.scala
+++ bot/src/main/scala/berner/bot/Main.scala
@@ -1,49 +1,33 @@
 package berner.bot
 
 import berner.logging.Logger
-import net.dv8tion.jda.api.JDABuilder
 import com.typesafe.config.ConfigFactory
+import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.requests.GatewayIntent
 import scalikejdbc.config.DBs
 
-import scala.annotation.tailrec
-import scala.util.control.Exception.allCatch
-
 object Main extends App with Logger {
+  private val config = ConfigFactory.load()
+  private val discordBotToken = config.getString("discord.token")
+
+  val api = JDABuilder
+    .createDefault(discordBotToken)
+    .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+    .enableIntents(GatewayIntent.GUILD_WEBHOOKS)
+    .enableIntents(GatewayIntent.GUILD_MESSAGES)
+    .addEventListeners(new Hub)
+    .addEventListeners(new Archiver)
+    .addEventListeners(new Ping)
+    .build()
+
   try {
-    val config = ConfigFactory.load()
-
     DBs.setupAll()
-
-    val discordBotToken = config.getString("discord.token")
-
-    @tailrec
-    def infinite(): Unit = {
-      allCatch.either {
-        val api = JDABuilder
-          .createDefault(discordBotToken)
-          .enableIntents(GatewayIntent.MESSAGE_CONTENT)
-          .enableIntents(GatewayIntent.GUILD_WEBHOOKS)
-          .enableIntents(GatewayIntent.GUILD_MESSAGES)
-          .addEventListeners(new Hub)
-          .addEventListeners(new Archiver)
-          .addEventListeners(new Ping)
-          .build()
-
-        api.awaitShutdown()
-      } match {
-        case Right(_) => // do nothing
-        case Left(e) =>
-          error("Botが異常終了", e)
-          info("3秒後に再起動")
-          Thread.sleep(3000)
-          infinite()
-      }
-    }
-
-    infinite()
+    api.awaitShutdown()
   } finally {
     DBs.closeAll()
-  }
 
+    val client = api.getHttpClient
+    client.connectionPool.evictAll()
+    client.dispatcher.executorService.shutdown()
+  }
 }
