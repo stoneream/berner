75f45c2b8fd3df8fd1b26b99c1dd448ab8d68cc1
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-10-17 23:33:38 +0900

    これいる？

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,6 @@ object Dependencies {
   // logging
   lazy val logging: Seq[ModuleID] = Seq(
     "ch.qos.logback" % "logback-classic" % "1.5.6",
-    "org.codehaus.janino" % "janino" % "3.1.12",
     "net.logstash.logback" % "logstash-logback-encoder" % "8.0"
   )
 
