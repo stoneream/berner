d6f5557779b49d556e000274511b7362f3944483
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2025-01-30 12:36:16 +0900

    テーブル追加

---
+++ db/migration/V1.1.1__hub_message_delete_queue.sql
@@ -0,0 +1,8 @@
+create table hub_message_delete_queue(
+  id serial primary key,
+  hub_message_mapping_id int not null,
+  status int not null comment '0: pending, 1: success, 2: failed',
+  created_at datetime not null,
+  updated_at datetime not null,
+  deleted_at datetime
+) engine = InnoDB default charset = utf8mb4;
