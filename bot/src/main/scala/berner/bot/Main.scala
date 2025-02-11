168a70580904848ee9ad8001e254dae6e768e137
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2025-02-11 10:43:51 +0900

    fiber化

--- bot/src/main/scala/berner/bot/Main.scala
+++ bot/src/main/scala/berner/bot/Main.scala
@@ -1,33 +1,28 @@
 package berner.bot
 
-import berner.logging.Logger
+import berner.daemon.HubDaemon
+import cats.effect.{ExitCode, IO, IOApp}
 import com.typesafe.config.ConfigFactory
-import net.dv8tion.jda.api.JDABuilder
-import net.dv8tion.jda.api.requests.GatewayIntent
 import scalikejdbc.config.DBs
 
-object Main extends App with Logger {
-  private val config = ConfigFactory.load()
-  private val discordBotToken = config.getString("discord.token")
+object Main extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = {
+    val loadConfig = IO {
+      val config = ConfigFactory.load()
+      config.getString("discord.token")
+    }
+    val setupDB = IO {
+      DBs.setupAll()
+    }
+    val closeDB = IO {
+      DBs.closeAll()
+    }
 
-  val api = JDABuilder
-    .createDefault(discordBotToken)
-    .enableIntents(GatewayIntent.MESSAGE_CONTENT)
-    .enableIntents(GatewayIntent.GUILD_WEBHOOKS)
-    .enableIntents(GatewayIntent.GUILD_MESSAGES)
-    .addEventListeners(new Hub)
-    .addEventListeners(new Archiver)
-    .addEventListeners(new Ping)
-    .build()
-
-  try {
-    DBs.setupAll()
-    api.awaitShutdown()
-  } finally {
-    DBs.closeAll()
-
-    val client = api.getHttpClient
-    client.connectionPool.evictAll()
-    client.dispatcher.executorService.shutdown()
+    (for {
+      discordBotToken <- loadConfig
+      _ <- setupDB
+      hubDaemonFiber <- HubDaemon.task(discordBotToken).start
+      _ <- hubDaemonFiber.join
+    } yield ()).guarantee(closeDB).as(ExitCode.Success)
   }
 }
---
+++ bot/src/main/scala/berner/daemon/HubDaemon.scala
@@ -0,0 +1,40 @@
+package berner.daemon
+
+import berner.bot.{Archiver, Hub, Ping}
+import cats.effect.IO
+import net.dv8tion.jda.api.requests.GatewayIntent
+import net.dv8tion.jda.api.{JDA, JDABuilder}
+
+object HubDaemon {
+  def task(discordBotToken: String): IO[Unit] = {
+    (for {
+      jda <- preExecute(discordBotToken)
+      _ <- execute(jda)
+      _ <- postExecute()
+    } yield ()).foreverM
+  }
+
+  private def preExecute(discordBotToken: String): IO[JDA] = IO {
+    JDABuilder
+      .createDefault(discordBotToken)
+      .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+      .enableIntents(GatewayIntent.GUILD_WEBHOOKS)
+      .enableIntents(GatewayIntent.GUILD_MESSAGES)
+      .addEventListeners(new Hub)
+      .addEventListeners(new Archiver)
+      .addEventListeners(new Ping)
+      .build()
+  }
+
+  private def execute(jda: JDA): IO[Boolean] = {
+    IO {
+      jda.awaitShutdown()
+    }.guarantee(IO {
+      val client = jda.getHttpClient
+      client.connectionPool.evictAll()
+      client.dispatcher.executorService.shutdown()
+    })
+  }
+
+  private def postExecute(): IO[Unit] = IO {}
+}
