dc8214401ca301e6399e8d68c1e7bd49aa1de73d
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2025-02-11 22:30:13 +0900

    例外発生時を考慮 & 削除完了・失敗時のステータス更新

--- bot/src/main/scala/berner/daemon/message_delete_daemon/MessageDeleteDamon.scala
+++ bot/src/main/scala/berner/daemon/message_delete_daemon/MessageDeleteDamon.scala
@@ -1,14 +1,17 @@
 package berner.daemon.message_delete_daemon
 
-import berner.database.HubMessageDeleteQueueReader
+import berner.database.{HubMessageDeleteQueueReader, HubMessageDeleteQueueWriter}
+import berner.logging.Logger
 import cats.effect.IO
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel
 import scalikejdbc.DB
 
+import java.time.OffsetDateTime
 import scala.jdk.CollectionConverters.SeqHasAsJava
+import scala.util.control.Exception.allCatch
 
-object MessageDeleteDamon {
+object MessageDeleteDamon extends Logger {
   def task(n: Int): IO[Unit] = {
     (preExecute() >> execute(???) >> postExecute()).foreverM
   }
@@ -16,20 +19,37 @@ object MessageDeleteDamon {
   private def preExecute(): IO[Unit] = IO {}
 
   private def execute(jda: JDA): IO[Unit] = IO {
-    val rows = DB.localTx { s => HubMessageDeleteQueueReader.pendings(100)(s) }
+    val rows = DB.localTx { s => HubMessageDeleteQueueReader.pendings(limit = 100)(s) }
+
     val groupedRows = rows
-      .groupBy { case (hmdq, hmm) => hmm.guildId } // サーバーごとにグルーピング
+      .groupBy { case (_, hmm) => hmm.guildId } // サーバーごとにグルーピング
       .map { case (guildId, rows) => (guildId, rows.groupBy(_._2.hubGuildMessageChannelId)) } // ハブチャンネルごとにグルーピング
 
-    // todo 例外発生時を考慮する
     groupedRows.map { case (guildId, hub) =>
       hub.map { case (hubMessageChannelId, queue) =>
         val messageIds = queue.map(_._2.hubMessageId)
-        for {
-          guild <- Option(jda.getGuildById(guildId))
-          hubChannel <- Option(guild.getChannelById(classOf[TextChannel], hubMessageChannelId))
-        } yield {
-          hubChannel.deleteMessagesByIds(messageIds.asJava)
+
+        allCatch.either {
+          for {
+            guild <- Option(jda.getGuildById(guildId))
+            hubChannel <- Option(guild.getChannelById(classOf[TextChannel], hubMessageChannelId))
+          } yield {
+            hubChannel.deleteMessagesByIds(messageIds.asJava)
+          }
+        } match {
+          case Left(e) =>
+            error("メッセージの削除中にエラーが発生しました", e)
+            val ids = queue.map(_._1.id)
+            val now = OffsetDateTime.now()
+            DB.localTx { s =>
+              HubMessageDeleteQueueWriter.markFailedByMessageIds(ids, now)(s)
+            }
+          case Right(_) =>
+            val ids = queue.map(_._1.id)
+            val now = OffsetDateTime.now()
+            DB.localTx { s =>
+              HubMessageDeleteQueueWriter.markDeleteByMessageIds(ids, now)(s)
+            }
         }
       }
     }
--- bot/src/main/scala/berner/database/HubMessageDeleteQueueWriter.scala
+++ bot/src/main/scala/berner/database/HubMessageDeleteQueueWriter.scala
@@ -3,6 +3,7 @@ package berner.database
 import berner.model.hub.{HubMessageDeleteQueue, HubMessageMapping}
 import scalikejdbc._
 
+import java.time.OffsetDateTime
 import scala.collection.IndexedSeq.iterableFactory
 
 object HubMessageDeleteQueueWriter {
@@ -26,6 +27,30 @@ object HubMessageDeleteQueueWriter {
     withSQL {
       insert.into(HubMessageMapping).namedValues(builder.columnsAndPlaceholders: _*)
     }.batch(builder.batchParams: _*).apply()
+  }
+
+  def markDeleteByMessageIds(ids: List[Long], now: OffsetDateTime)(session: DBSession): Unit = {
+    implicit val s: DBSession = session
+
+    withSQL {
+      update(HubMessageDeleteQueue)
+        .set(column.status -> 1) // 削除済み
+        .set(column.updatedAt -> now)
+        .where
+        .in(column.id, ids)
+    }.update.apply()
+  }
+
+  def markFailedByMessageIds(ids: List[Long], now: OffsetDateTime)(session: DBSession): Unit = {
+    implicit val s: DBSession = session
+
+    withSQL {
+      update(HubMessageDeleteQueue)
+        .set(column.status -> 2) // 削除失敗
+        .set(column.updatedAt -> now)
+        .where
+        .in(column.id, ids)
+    }.update.apply()
   }
 
 }
